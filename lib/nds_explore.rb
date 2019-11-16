596bf4c348c4a1877ab0616c4a49e7bd469da1d9
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-16 21:41:49 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,6 @@ end
 def print_first_directors_movie_titles
   row_index = 0 
   while row_index < print_first_directors_movie_titles.length do 
-    puts print_first_directors_movie_titles
+    puts print_first_directors_movie_titles[0][1]
     row_index +=1
 end
